869d2da49c1733af233860d1cca98d10bcecaab5
Maxime Maussion <maximemaussion@macbook-air-de-maxime.home>
2024-10-10 14:00:14 +0200

    Refacto type.ts, by deletiing this folder and placing types in the folder  they were needed

--- App.tsx
+++ App.tsx
@@ -2,9 +2,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Homepage from "./src/pages/Homepage";
 import Bookpage from "./src/pages/Bookpage";
-import { StackParamList } from "./src/types";
 import { DataContextProvider } from "./src/context/FetchContext";
 import Chapterpage from "./src/pages/Chapterpage";
+
+export type StackParamList = {
+  Home: undefined;
+  Bookpage: { bookId: number; displayTitle: string };
+  Chapterpage: { chapterId: number; title: string };
+};
 
 const Stack = createNativeStackNavigator<StackParamList>();
 
